10c0eb04f37b02bbc883792b97da193a5f1d6168
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-12-26 11:42:28 -0300

    add types in functions

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-# Init Tokenizer from Hugging Face Transformers library
-tokenizer = T5Tokenizer.from_pretrained('t5-small', legacy=False)
+def init_tokenizer(model_name: str = 't5-small') -> T5Tokenizer:
+    return T5Tokenizer.from_pretrained(model_name, legacy=False)
 
-# load model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = T5ForConditionalGeneration.from_pretrained('cssupport/t5-small-awesome-text-to-sql')
-model = model.to(device)
-model.eval()
+def load_model(model_name='cssupport/t5-small-awesome-text-to-sql'):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = T5ForConditionalGeneration.from_pretrained(model_name)
+    model = model.to(device)
+    model.eval()
+    return model, device
 
-def generate_sql(input_prompt):
+def generate_sql(model: T5ForConditionalGeneration, tokenizer: T5Tokenizer, device: torch.device, input_prompt: str) -> str:
     inputs = tokenizer(input_prompt, padding=True, truncation=True, return_tensors="pt").to(device)
     
     with torch.no_grad():
@@ -19,13 +20,19 @@ def generate_sql(input_prompt):
     
     return generated_sql
 
-# database schema
-schema = "CREATE TABLE student_course_attendance (student_id VARCHAR); CREATE TABLE students (student_id VARCHAR)"
-# query to generate SQL
-query = "count the number of students who attended the course"
+def main():
+    # Initialize tokenizer and model
+    tokenizer = init_tokenizer()
+    model, device = load_model()
 
-input_prompt = "tables:\n" + schema + "\nquery:\n" + query
+    # Database schema and query
+    schema = "CREATE TABLE student_course_attendance (student_id VARCHAR); CREATE TABLE students (student_id VARCHAR)"
+    query = "count the number of students who attended the course"
+    input_prompt = "tables:\n" + schema + "\nquery:\n" + query
 
-generated_sql = generate_sql(input_prompt)
+    # Generate SQL
+    generated_sql = generate_sql(model, tokenizer, device, input_prompt)
+    print(f"The generated SQL query is: {generated_sql}")
 
-print(f"The generated SQL query is: {generated_sql}")
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
-# Generate Querys from natural languages
+# Gera querys apartir de linguagem natural
+Um projeto de teste para praticar o uso de IA com Querys
+
+# Instalação
+``` shell
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+# modelo usado
+ - cssupport t5-small-awesome-text-to-sql 
